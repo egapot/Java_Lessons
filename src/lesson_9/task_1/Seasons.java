48165e7749e2f71a5b93a63feab758d245f2c864
egapot <57811970+egapot@users.noreply.github.com>
2020-07-11 17:53:50 +0300

    Java Lesson 9.1 done!

---
+++ src/lesson_9/task_1/Seasons.java
@@ -0,0 +1,5 @@
+package lesson_9.task_1;
+
+public enum Seasons {
+    Winter, Spring, Summer, Autumn
+}
---
+++ src/lesson_9/task_1/WrongInputConsoleParametersException.java
@@ -0,0 +1,8 @@
+package lesson_9.task_1;
+
+public class WrongInputConsoleParametersException extends Exception {
+
+    public WrongInputConsoleParametersException(String message) {
+        super(message);
+    }
+}
